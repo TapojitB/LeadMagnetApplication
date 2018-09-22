b560c2a67b37d7bdc891129198427a755ac1d47e
Tapojit Bhattacharya <tapojit.bhattacharya@gmail.com>
2018-09-22 15:59:20 +0530

    Collection Utils

--- product-filter-service/src/main/java/com/evergreenprogrammers/microservices/productfilterservice/controller/ProductFilterController.java
+++ product-filter-service/src/main/java/com/evergreenprogrammers/microservices/productfilterservice/controller/ProductFilterController.java
@@ -7,8 +7,8 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
