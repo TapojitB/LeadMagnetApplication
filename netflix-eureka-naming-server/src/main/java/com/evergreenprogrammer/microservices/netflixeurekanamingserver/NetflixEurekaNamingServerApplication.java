6d70115785c30035d74f76f29b846a16abcef030
Tapojit Bhattacharya <tapojit.bhattacharya@gmail.com>
2018-10-27 13:18:09 +0530

    Refactored LeadMagnet Eureka

--- netflix-eureka-naming-server/src/main/java/com/evergreenprogrammer/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
+++ netflix-eureka-naming-server/src/main/java/com/evergreenprogrammer/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -1,4 +1,4 @@
-package com.evergreenprogrammers.microservices.netflixeurekanamingserver;
+package com.evergreenprogrammer.microservices.netflixeurekanamingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- netflix-eureka-naming-server/src/test/java/com/evergreenprogrammer/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
+++ netflix-eureka-naming-server/src/test/java/com/evergreenprogrammer/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.evergreenprogrammers.microservices.netflixeurekanamingserver;
+package com.evergreenprogrammer.microservices.netflixeurekanamingserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
